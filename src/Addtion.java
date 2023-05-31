2813e553cbbbba1470f30d98510091db205178e7
MKBmanas <manaskumar347@gmail.com>
2023-05-31 15:03:07 +0530

    addition project

---
+++ src/Addtion.java
@@ -0,0 +1,13 @@
+
+public class Addtion {
+
+	public static void main(String[] args) {
+		int num1=100;
+		int num2=200;
+		int result;
+		result=num1+num2;
+		System.out.println(result);
+
+	}
+
+}
