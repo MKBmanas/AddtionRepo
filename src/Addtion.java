bec38b1ee732b2d0b2f54d2a203dd5bcde2d3f72
MKBmanas <132010539+MKBmanas@users.noreply.github.com>
2023-05-31 15:23:44 +0530

    Update Addtion.java
    code modified

--- src/Addtion.java
+++ src/Addtion.java
@@ -4,9 +4,11 @@ public class Addtion {
 	public static void main(String[] args) {
 		int num1=100;
 		int num2=200;
-		int result;
-		result=num1+num2;
-		System.out.println(result);
+		int add;
+		int mul;
+		add=num1+num2;
+		mul=num1*num2;
+		System.out.println(add+mul);
 
 	}
 
